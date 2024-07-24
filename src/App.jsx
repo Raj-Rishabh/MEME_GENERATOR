84541cbec2ac92212aae3f2fb4724128ba2861e8
Rishabh Raj <rajrishabh374@gmail.com>
2024-07-24 15:44:39 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+
+export default function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Body />
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import png from "../assets/icon1.png"
+
+
+export default function Navbar(){
+    return(
+        <div className="nav container">
+            <img src={png} alt="" srcset="" />
+            <h1>Meme Generator</h1>
+
+        </div>
+    )
+}
